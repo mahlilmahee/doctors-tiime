d7ccc2be587006d55e5a67b9c34710b7ab822f9a
acc username <acc email>
2021-10-31 19:36:26 +0600

    adf

--- src/Component/ManageOrder/ManageOrder.js
+++ src/Component/ManageOrder/ManageOrder.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import useAuth from './../Home/Context/UseAuth';
 import { Container,Row,Col } from 'react-bootstrap';
 import SingleOrder from './SingleOrder';
+import { Button } from 'react-bootstrap';
 
 const ManageOrder = () => {
     const [order,setOrder]=useState([]);
@@ -12,8 +13,28 @@ const ManageOrder = () => {
         fetch(`http://localhost:4000/tourinfo/${user.email}`)
         .then(res=>res.json())
         .then(data=>{setOrder(data)
-            console.log(data)})
-    },[])
+           })
+    },[]);
+    const handlingdeleted=()=>{
+        const decision=window.confirm('Are you sure to cancel your order ')
+        if(decision){
+            let id; 
+            for(const id of  order){
+                console.log(id._id);
+            
+            fetch(`http://localhost:4000/tourpak/${id._id}`,{
+              method:"DELETE"
+              })
+          .then(res=>res.json())
+          .then(data=>{
+              if(data.deletedCount>0){
+                 const rest=order.filter(data=>id._id!==data._id);
+                 setOrder(rest)
+              }
+          })
+        }}
+          
+      }
     return ( 
         <div>
                 
@@ -23,23 +44,16 @@ const ManageOrder = () => {
 <div className="container">
     <div className="row">
         {
-    order.map(data=><SingleOrder Key={data._id} data={data}></SingleOrder>)
+
+  order.map(data=><SingleOrder Key={data._id} data={data}>   <Button onClick={handlingdeleted}>Cancel order </Button></SingleOrder>)
+
 }
     </div>
 
 
 </div>
-             {/* <Container>
-  
-          <Row>
-          {
-              order.map(data=><Col> {data._id}</Col>)
-          }
-          
-           </Row>
+            
 
-
-          </Container> */}
 
         </div>
     );
--- src/Component/ManageOrder/SingleOrder.js
+++ src/Component/ManageOrder/SingleOrder.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+
 import { useLocation } from 'react-router-dom';
-import './SingleOrder.css'
+import './SingleOrder.css';
+import { Button } from 'react-bootstrap';
 const SingleOrder = (props) => {
-   const {_id,name,email,status,tourName}=props.data;
-   const location=useLocation();
-  const handlingdeleted=()=>{
-    const decision=window.confirm('Are you sure to cancel your order ')
-    if(decision){
-        fetch(`http://localhost:4000/tourpak/${_id}`,{
-          method:"DELETE"
-          })
-      .then(res=>res.json())
-      .then(data=>{
-          if(data.deletedCount>0){
-            location.reload();
-          }
-      })
-    }
-      
-  }
+   const {name,email,status,tourName}=props.data;
+ 
+
     return (
         <div className=" col-12 col-lg-6">
         <div className="single  ">
@@ -27,7 +14,7 @@ const SingleOrder = (props) => {
           <h5>Order:{tourName}</h5>
           <h5>Status:{status}</h5>
           <h5>Email:{email}</h5>
-          <Button onClick={handlingdeleted}>Cancel your order </Button>
+          {props.children}
         </div>
         </div>
     );
