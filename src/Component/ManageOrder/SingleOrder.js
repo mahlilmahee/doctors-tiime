be6011feb369560c1a82f51457952aa9b887362b
acc username <acc email>
2021-10-31 19:13:12 +0600

    deleting

--- src/Component/ManageOrder/SingleOrder.js
+++ src/Component/ManageOrder/SingleOrder.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import './SingleOrder.css'
 const SingleOrder = (props) => {
    const {_id,name,email,status,tourName}=props.data;
-
+   const location=useLocation();
+  const handlingdeleted=()=>{
+    const decision=window.confirm('Are you sure to cancel your order ')
+    if(decision){
+        fetch(`http://localhost:4000/tourpak/${_id}`,{
+          method:"DELETE"
+          })
+      .then(res=>res.json())
+      .then(data=>{
+          if(data.deletedCount>0){
+            location.reload();
+          }
+      })
+    }
+      
+  }
     return (
         <div className=" col-12 col-lg-6">
         <div className="single  ">
@@ -11,7 +27,7 @@ const SingleOrder = (props) => {
           <h5>Order:{tourName}</h5>
           <h5>Status:{status}</h5>
           <h5>Email:{email}</h5>
-          <Button>Cancel your order </Button>
+          <Button onClick={handlingdeleted}>Cancel your order </Button>
         </div>
         </div>
     );
