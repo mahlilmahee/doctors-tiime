768ac95db66dc1388a0820f0c4d5ce5b03ebd271
acc username <acc email>
2021-10-31 16:27:26 +0600

    ad

--- src/Component/ManageOrder/ManageOrder.js
+++ src/Component/ManageOrder/ManageOrder.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import useAuth from './../Home/Context/UseAuth';
 
 const ManageOrder = () => {
+    const [order,setOrder]=useState([]);
+    const {user}=useAuth();
+    useEffect(()=>{
+        fetch(`http://localhost:4000/tourinfo/${user.email}`)
+        .then(res=>res.json())
+        .then(data=>{setOrder(data)
+            console.log(data)})
+    },[])
     return (
         <div>
                 <h2> This is manage order by brother </h2>
+                <h3> WE have got {order.length}</h3>
+
         </div>
     );
 };
